2411561c4a13124b7c481e5658ab8724713358ed
kshatta <70550600+kshatta@users.noreply.github.com>
2021-09-26 09:54:22 -0400

    Create SelectCarServlet.java

---
+++ src/main/java/com.sample/SelectCarServlet.java
@@ -0,0 +1,36 @@
+package com.sample;
+
+import com.sample.model.CarMake;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+
+@WebServlet(
+        name = “selectCarServlet",
+        urlPatterns = "/SelectCar"
+)
+public class SelectCarServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String carType = req.getParameter("Type");
+
+        CarService carService = new CarService();
+        CarType c = CarType.valueOf(carType);
+
+        List carBrands = carService.getAvailableBrands(c);
+
+        req.setAttribute("brands", carBrands);
+        RequestDispatcher view = req.getRequestDispatcher("result.jsp");
+        view.forward(req, resp);
+
+    }
+}
