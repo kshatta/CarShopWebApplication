5a1616b0bbb45abb077876769f17243254dc1e37
kshatta <70550600+kshatta@users.noreply.github.com>
2021-09-26 12:40:20 -0400

    Update SelectCarServlet.java

--- src/main/java/com.sample/SelectCarServlet.java
+++ src/main/java/com.sample/SelectCarServlet.java
@@ -13,24 +13,18 @@ import java.util.List;
 
 
 @WebServlet(
-        name = “selectCarServlet",
+        name = "selectCarServlet",
         urlPatterns = "/SelectCar"
 )
 public class SelectCarServlet extends HttpServlet {
-
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         String carType = req.getParameter("Type");
-
         CarService carService = new CarService();
         CarType c = CarType.valueOf(carType);
-
         List carBrands = carService.getAvailableBrands(c);
-
         req.setAttribute("brands", carBrands);
         RequestDispatcher view = req.getRequestDispatcher("result.jsp");
         view.forward(req, resp);
-
     }
 }
